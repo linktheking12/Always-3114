79746b3255354b5a59a1806e6854e48d56b95319
Fernando Chicas <fchicas@hotmail.com>
2023-11-13 00:44:47 -0500

    Add project files.

---
+++ Always 3114/Config.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Always_3114
+{
+    public class Config
+    {
+        [Description("A test boolean")]
+        public bool TestBoolean { get; set; } = true;
+    }
+}
---
+++ Always 3114/Plugin.cs
@@ -0,0 +1,24 @@
+﻿using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Core.Interfaces;
+using PluginAPI.Enums;
+using PluginAPI.Events;
+using System;
+
+
+namespace Always_3114
+{
+    public class MainClass
+    {
+
+        [PluginConfig]
+        public Config Config;
+
+        [PluginEntryPoint("Always_3114", "1.0.0", "Plugin that forces a 3114 in a game if there isn't one already", "linktheking")]
+        public void Init()
+        {
+            Log.Info("Hello World");
+        }
+
+    }
+}
